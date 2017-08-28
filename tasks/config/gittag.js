c3467b47ac90a5e2bb56b40cf1262d7def2e767f
Snehil Modani <snehil.m@turtlemint.com>
2017-08-28 14:42:23 +0530

    code changes commited: 1.0.17

--- tasks/config/gittag.js
+++ tasks/config/gittag.js
@@ -5,7 +5,7 @@ var version = require('../../package.json').version;
   grunt.config.set('gittag', {
     version: {
       options: {
-        tag: version,
+        tag: 'v' + require('../../package.json').version,
         message: 'Bumped to version: ' + version
       }
     }
