b60fd38b580606d42b397d2344c926bf303f07c3
Snehil Modani <snehil.m@turtlemint.com>
2018-01-30 19:36:43 +0530

    code changes commited: 1.0.48

--- src/dir/form-component.dir.js
+++ src/dir/form-component.dir.js
@@ -72,7 +72,7 @@
           if(ViewModel.config.autocompleteConfig && ViewModel.config.autocompleteConfig.displayKey && !ViewModel.config.autocompleteConfig.valueKey) {
             ViewModel.config.autocompleteConfig.valueKey = ViewModel.config.autocompleteConfig.displayKey;
           }
-          var results = ViewModel.config.autocompleteConfig.querySearch(ViewModel.model[ViewModel.config.code]).$$state.value || [];
+          var results = ViewModel.config.autocompleteConfig.querySearch(ViewModel.model[ViewModel.config.code], ViewModel.model).$$state.value || [];
           if(results && results.constructor === Array && results.length === 1) {
             ViewModel.config.autocompleteConfig.selectedItem = results[0];
           } else {
