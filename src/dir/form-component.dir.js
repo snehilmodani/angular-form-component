cf96a7eec1716496e70932d439d4094251776444
saumin kirve <saumin.k@turtlemint.com>
2018-05-21 16:52:21 +0530

    added checkbox with input

--- src/dir/form-component.dir.js
+++ src/dir/form-component.dir.js
@@ -121,6 +121,9 @@
         } else if (ViewModel.config && ViewModel.config.type === 'checkbox' && ViewModel.model[ViewModel.config.code]) {
           // Hack for string/other truthy values
           ViewModel.model[ViewModel.config.code] = true;
+        }else if (ViewModel.config && ViewModel.config.type === 'checkboxWithInput' && ViewModel.model[ViewModel.config.code]) {
+          // Hack for string/other truthy values
+          ViewModel.model[ViewModel.config.code] = true;
         } else if (ViewModel.config && ViewModel.config.type === 'select') {
           if(!ViewModel.config.selectConfig) {
             ViewModel.config.selectConfig = {};
